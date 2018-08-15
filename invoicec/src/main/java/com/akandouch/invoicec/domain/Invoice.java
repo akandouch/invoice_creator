58aecf03d3cca82e4f4134c38dbcae7cae56a897
Akandouch <zaide.akandouch@cgi.com>
2018-08-15 14:39:51 +0200

    customer informations

--- invoicec/src/main/java/com/akandouch/invoicec/domain/Invoice.java
+++ invoicec/src/main/java/com/akandouch/invoicec/domain/Invoice.java
@@ -8,6 +8,20 @@ public class Invoice {
 	private String title;
 	private List<Item> items;
 	
+	private String cust_name;
+	private String cust_street;
+	private String cust_city;
+	private String cust_postcode;
+	private String cust_vat;
+	
+	private String name;
+	private String street;
+	private String city;
+	private String postcode;
+	private String vat;
+	
+	
+	
 	
 	public Long getId() {
 		return id;
@@ -27,6 +41,55 @@ public class Invoice {
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
+	public String getCust_street() {
+		return cust_street;
+	}
+	public void setCust_street(String cust_street) {
+		this.cust_street = cust_street;
+	}
+	public String getCust_city() {
+		return cust_city;
+	}
+	public void setCust_city(String cust_city) {
+		this.cust_city = cust_city;
+	}
+	public String getCust_postcode() {
+		return cust_postcode;
+	}
+	public void setCust_postcode(String cust_postcode) {
+		this.cust_postcode = cust_postcode;
+	}
+	public String getCust_vat() {
+		return cust_vat;
+	}
+	public void setCust_vat(String cust_vat) {
+		this.cust_vat = cust_vat;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getVat() {
+		return vat;
+	}
+	public void setVat(String vat) {
+		this.vat = vat;
+	}
+	
 	
 	
 }
--- invoicec/src/main/java/com/akandouch/invoicec/domain/Item.java
+++ invoicec/src/main/java/com/akandouch/invoicec/domain/Item.java
@@ -6,6 +6,14 @@ public class Item {
 	private String description;
 	private Integer unit;
 	private Double amount;
+	
+	
+	private String project;
+    private String period;
+    private String nature;
+    private Float days;
+    private Float rate;
+    
 	public Long getId() {
 		return id;
 	}
@@ -30,7 +38,34 @@ public class Item {
 	public void setAmount(Double amount) {
 		this.amount = amount;
 	}
-	
-	
-
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+	public String getNature() {
+		return nature;
+	}
+	public void setNature(String nature) {
+		this.nature = nature;
+	}
+	public Float getDays() {
+		return days;
+	}
+	public void setDays(Float days) {
+		this.days = days;
+	}
+	public Float getRate() {
+		return rate;
+	}
+	public void setRate(Float rate) {
+		this.rate = rate;
+	}
 }
