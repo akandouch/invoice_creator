dd59624115f05d125f6156b73ead0a910a26663f
Akandouch <zaide.akandouch@cgi.com>
2018-10-10 16:09:54 +0200

    test

--- invoicec/src/main/java/com/akandouch/invoicec/domain/Invoice.java
+++ invoicec/src/main/java/com/akandouch/invoicec/domain/Invoice.java
@@ -8,19 +8,8 @@ public class Invoice {
 	private String title;
 	private List<Item> items;
 	
-	private String cust_name;
-	private String cust_street;
-	private String cust_city;
-	private String cust_postcode;
-	private String cust_vat;
-	
-	private String name;
-	private String street;
-	private String city;
-	private String postcode;
-	private String vat;
-	
-	
+	private InvoiceProfile invoicer;
+	private InvoiceProfile invoiced;
 	
 	
 	public String getId() {
@@ -41,65 +30,17 @@ public class Invoice {
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
-	public String getCust_street() {
-		return cust_street;
+	public InvoiceProfile getInvoicer() {
+		return invoicer;
 	}
-	public void setCust_street(String cust_street) {
-		this.cust_street = cust_street;
+	public void setInvoicer(InvoiceProfile invoicer) {
+		this.invoicer = invoicer;
 	}
-	public String getCust_city() {
-		return cust_city;
+	public InvoiceProfile getInvoiced() {
+		return invoiced;
 	}
-	public void setCust_city(String cust_city) {
-		this.cust_city = cust_city;
-	}
-	public String getCust_postcode() {
-		return cust_postcode;
-	}
-	public void setCust_postcode(String cust_postcode) {
-		this.cust_postcode = cust_postcode;
-	}
-	public String getCust_vat() {
-		return cust_vat;
-	}
-	public void setCust_vat(String cust_vat) {
-		this.cust_vat = cust_vat;
-	}
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getPostcode() {
-		return postcode;
-	}
-	public void setPostcode(String postcode) {
-		this.postcode = postcode;
-	}
-	public String getVat() {
-		return vat;
-	}
-	public void setVat(String vat) {
-		this.vat = vat;
-	}
-	public String getCust_name() {
-		return cust_name;
-	}
-	public void setCust_name(String cust_name) {
-		this.cust_name = cust_name;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
+	public void setInvoiced(InvoiceProfile invoiced) {
+		this.invoiced = invoiced;
 	}
 	
 	
