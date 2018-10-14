397d009b1ff4197340797894444eabc4ad228c33
Nordine Bittich <contact@bittich.be>
2018-10-14 19:02:42 +0200

    fix profile

--- invoicec/src/main/java/com/akandouch/invoicec/domain/InvoiceProfile.java
+++ invoicec/src/main/java/com/akandouch/invoicec/domain/InvoiceProfile.java
@@ -7,42 +7,54 @@ public class InvoiceProfile {
 	private String lastname;
 	private Boolean active;
 	
-	private String VAT;
+	private String vat;
 	private Address address;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-	public String getVAT() {
-		return VAT;
-	}
-	public void setVAT(String vAT) {
-		VAT = vAT;
-	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
+
 	public Boolean getActive() {
 		return active;
 	}
+
 	public void setActive(Boolean active) {
 		this.active = active;
+	}
+
+	public String getVat() {
+		return vat;
+	}
+
+	public void setVat(String vat) {
+		this.vat = vat;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 }
