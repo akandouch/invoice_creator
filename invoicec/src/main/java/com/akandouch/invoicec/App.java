c6b4965dc0467f7fe2431ee55d3d79244040fda3
Akandouch <zaide.akandouch@cgi.com>
2018-10-19 10:53:06 +0200

    cleaning

--- invoicec/src/main/java/com/akandouch/invoicec/App.java
+++ invoicec/src/main/java/com/akandouch/invoicec/App.java
@@ -30,32 +30,5 @@ public class App
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
-    	/*
-    	
-    	File file = new File("C:\\\\workspace\\\\test.pdf");
-    	
-		try(FileOutputStream os = new FileOutputStream(file)) {
-			Document pdf = new Document();
-			PdfWriter.getInstance(pdf, os);
-			pdf.open();
-			Rectangle r = new Rectangle(150, 150);
-			PdfPTableHeader tableh = new PdfPTableHeader();
-			PdfPTable table = new PdfPTable(5);
-            table.setWidthPercentage(60);
-            table.setWidths(new int[]{3, 3, 3, 3, 3});
-			table.addCell("test");
-			table.addCell("test");
-			table.addCell("test");
-			table.addCell("test");
-			table.addCell("test");
-	    	pdf.add(table);
-	    	pdf.close();
-		} catch (FileNotFoundException fileEx) {
-			System.out.println("1 : " + fileEx.getMessage());
-		} catch (DocumentException docEx) {
-			System.out.println("2 : " + docEx.getMessage());
-		} catch (IOException e) {
-			System.out.println("3 : " + e.getMessage());
-		}*/
     }
 }
